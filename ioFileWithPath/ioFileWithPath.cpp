a94697de83aa732cde2596a08b642505ad32d121
RamaRamdani03 <ramaramdani449@gamil.com>
2023-06-21 11:10:05 +0700

    menambahkan variabel dengan menggunakan tipe data string baris dan nama file

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -6,23 +6,3 @@ using namespace std;
 int main() {
 	string baris;
 	string NamaFile;
-
-	cout << "Masukan Nama File : ";
-	cin >> NamaFile;
-
-	// membuka file dalam mode menulis
-	ofstream outfile;
-	// menunjuk ke sebuah nama file
-	outfile.open(NamaFile + ".txt", ios::out);
-
-	cout << ">= Menulis File, \'q\' untuk keluar" << endl;
-
-	// unlimited loop untuk menulis
-	while (true) {
-		cout << "- ";
-		// mendapatkan setiap karakter dalam satu baris
-		getline(cin, baris);
-		// loop akan berhenti jika anda memasukan karakter q
-
-	}
-}
