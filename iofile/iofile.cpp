06d4d138328974beb43f8a87e3cf70cf83acef74
RamaRamdani03 <ramaramdani449@gamil.com>
2023-06-21 11:01:47 +0700

    jika file ada maka melakukan perulangan setiap baris

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -32,3 +32,7 @@ int main() {
 	infile.open("Contohfile.txt");
 
 	cout << endl << ">= Membuka dan Membaca file " << endl;
+	// jika file ada maka
+	if (infile.is_open())
+	{
+		// melakukan perulangan setiap baris
