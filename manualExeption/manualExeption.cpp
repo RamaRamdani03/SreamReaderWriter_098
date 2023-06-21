71e9f291580582073226cf4130fcd9c48e432d63
RamaRamdani03 <ramaramdani449@gamil.com>
2023-06-21 11:47:06 +0700

    manual exception standar

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() 
+int main()
 {
 	try {
 		cout << "Selamat Belajar di Prodi TI UMY" << endl;
-		throw 0.5;	//melemparkan sebuah integer maka
+		throw 0.5; //melemparkan sebuah integer maka
 		cout << "Pernyataan tidak akan dieksekusi" << endl;
 	}
-	catch(int a) {
-		// blok ini akan di eksekusi
-		cout << "Pengecualian akan di ekesekusi" << endl;
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
 	}
 	catch (...) {
-		/*Jika selain integer maka block ini akan dieksekusi*/
-		cout << "default Pengecualian dieksekusi" << endl;
+		/*jika selain integer maka block ini akan dieksekusi*/
+		cout << "default Pengecualian diexsekusi" << endl;
 	}
 
 	return 0;
